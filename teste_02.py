73b2275d1c43c90baf0aa130e4ec8835c0abc824
Alexsandro0101 <alexsandrosouzacruz8@gmail.com>
2025-05-22 22:05:17 -0300

    Delete teste_02.py

--- teste_02.py
+++
@@ -1,8 +0,0 @@
-
-import requests
-
-def test_nome_usuario():
-    response = requests.get("https://reqres.in/api/users/2")
-    json = response.json()
-    nome = json['data']['first_name']
-    assert nome == "Janet"
